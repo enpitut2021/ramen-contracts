f7566e7db4133c2a3262fdbf8b131efe74183f5a
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-07-29 11:19:00 +0900

    fix error

--- test/proxy.js
+++ test/proxy.js
@@ -3,7 +3,7 @@ const Proxy = artifacts.require("Proxy");
 contract("Proxy", (accounts) => {
   it("should proxy a function call", async (accounts) => {
     const instance = await Proxy.deployed();
-    const result = await instance.verifySignature([0x00], [0x00]);
+    const result = await instance.verifySignature("0x0001", "0x0002");
     assert.equal(result, true);
   });
 });
