0a614f7087bbbc2079e926c30a695e233a7e66db
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-07-29 11:31:32 +0900

    bytes to uint

--- contracts/proxy.sol
+++ contracts/proxy.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.6;
 contract Proxy {
     constructor() {}
 
-    function verifySignature(bytes memory signature, bytes memory message)
+    function verifySignature(uint256 signature, uint256 message)
         public
         pure
         returns (bool)
     {
         require(
-            signature.length == message.length,
-            "Signature and message must be the same length" // 嘘です
+            signature == message,
+            "Invalid signature" // 嘘です
         );
         return true;
     }
--- test/proxy.js
+++ test/proxy.js
@@ -1,9 +1,14 @@
 const Proxy = artifacts.require("Proxy");
 
 contract("Proxy", (accounts) => {
+  it("should deploy a Proxy", async () => {
+    const proxy = await Proxy.new();
+    const instance = await proxy.deployed();
+    assert.equal(proxy.address, await proxy.address.call(), "助けて");
+  });
   it("should proxy a function call", async (accounts) => {
     const instance = await Proxy.deployed();
-    const result = await instance.verifySignature("0x0001", "0x0002");
-    assert.equal(result, true);
+    const result = await instance.verifySignature("0x01", "0x02");
+    assert.equal(result, true, "何これ");
   });
 });
