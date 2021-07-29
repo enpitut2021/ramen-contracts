abfe346bde2390f066b71cb7021766de7747aa3b
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-07-29 11:53:04 +0900

    fix bug

--- test/proxy.js
+++ test/proxy.js
@@ -3,12 +3,13 @@ const Proxy = artifacts.require("Proxy");
 contract("Proxy", (accounts) => {
   it("should deploy a Proxy", async () => {
     const proxy = await Proxy.new();
-    const instance = await proxy.deployed();
-    assert.equal(proxy.address, await proxy.address.call(), "助けて");
-  });
-  it("should proxy a function call", async (accounts) => {
     const instance = await Proxy.deployed();
-    const result = await instance.verifySignature("0x01", "0x02");
-    assert.equal(result, true, "何これ");
+    assert.ok(instance.address, "失敗");
+  });
+  it("should proxy a function call", async () => {
+    const proxy = await Proxy.new();
+    const instance = await Proxy.deployed();
+    const result = await instance.verifySignature("0x01", "0x01");
+    assert.ok(result, "何これ");
   });
 });
