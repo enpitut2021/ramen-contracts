70e6ffacac8c51b93db8534007be5cff516fdddc
中島柚斗 <yuto@MacBook-Air.local>
2021-07-29 14:47:10 +0900

    実装の部分がお互いにわかるように細かく分けてみたよ（それだけ）

--- contracts/proxy.sol
+++ contracts/proxy.sol
@@ -7,6 +7,17 @@ contract Proxy {
         bytes memory pubkey,
         bytes memory message,
         bytes memory signature
+
+        // RSAVP1の適用を書く場所
+
+        // メッセージ代表mをエンコードされたメッセージに変換する場所
+
+        // EMSAーPSS検証を適用するところ
+
+        // Result = "consistent"の場合、 "validsignature"を出力します。
+        //   それ以外の場合は、「無効な署名」を出力する。
+
+
     ) public pure returns (bool) {
         require(
             true,
