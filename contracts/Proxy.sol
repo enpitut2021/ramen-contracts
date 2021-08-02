491bd7860bf4954c4296189d29d54c8096404943
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-08-03 01:47:07 +0900

    add ether methods(untested)

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -36,4 +36,54 @@ contract Proxy {
         transactionCount++;
         return ret;
     }
+
+    function proxyWithValue(
+        address target, // address of the target contract
+        uint256 nonce, // nonce
+        uint256 value, // the value to send to the target contract
+        bytes memory argument, // the argument(calldata) to send to the target contract
+        bytes memory signature // the signature of `abi.encode(target, argument, nonce)`
+    ) public returns (bytes memory) {
+        // begin requirements check
+        require(nonce == transactionCount, "Invalid nonce");
+        bytes memory toHash = abi.encode(target, argument, value, nonce);
+        require(
+            Verifier.verifySignature(
+                modulus,
+                exponent,
+                sha256(toHash),
+                signature
+            ),
+            "Invalid signature"
+        );
+        // begin state mutation
+        (bool success, bytes memory ret) = target.call{value: value}(argument);
+        require(success, "Target contract execution failed");
+        transactionCount++;
+        return ret;
+    }
+
+    function proxyForEther(
+        address payable target, // address of the destination account
+        uint256 nonce, // nonce
+        uint256 value, // the value to send to destination
+        bytes memory signature // the signature of `abi.encode(target, argument, nonce)`
+    ) public {
+        // begin requirements check
+        require(nonce == transactionCount, "Invalid nonce");
+        bytes memory toHash = abi.encode(target, value, nonce);
+        require(
+            Verifier.verifySignature(
+                modulus,
+                exponent,
+                sha256(toHash),
+                signature
+            ),
+            "Invalid signature"
+        );
+        // begin state mutation
+        bool success = target.send(value);
+        require(success, "Transfer failed");
+        transactionCount++;
+    }
 }
